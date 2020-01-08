36f11e6407cf953ae8c810d22bb14a4920601cc1
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-01-07 22:00:16 -0500

    restors backup

--- db/init_db.sh
+++ db/init_db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mysql -u root -p${MYSQL_ROOT_PASSWORD} < /docker-entrypoint-initdb.d/init_lychee_db.sql
 mysql -u root -p${MYSQL_ROOT_PASSWORD} < /docker-entrypoint-initdb.d/init_test_db.sql
+mysql -u root -p${MYSQL_ROOT_PASSWORD} lychee < /backup_0.sql
