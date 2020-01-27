9a06dfa2c4752d6e4fb3b4d5f572463f3fa24bb3
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-01-26 22:41:26 -0500

    home comp test fix

--- rumbaugh-art-app/src/app/home/home.component.spec.ts
+++ rumbaugh-art-app/src/app/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        NgbCarouselModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
