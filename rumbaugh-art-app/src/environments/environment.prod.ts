1977e3b951ac6b3a0b80016299a730d64ce1a9fc
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-03-08 10:35:39 -0400

    fix prod url

--- rumbaugh-art-app/src/environments/environment.prod.ts
+++ rumbaugh-art-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  serverUrl: 'ec2-user@ec2-34-227-83-189.compute-1.amazonaws.com',
+  serverUrl: 'ec2-34-227-83-189.compute-1.amazonaws.com',
   serverPort: '80',
   photoServerPort: '8080',
-  photoServerUrl: 'ec2-user@ec2-34-227-83-189.compute-1.amazonaws.com:8080'
+  photoServerUrl: 'ec2-34-227-83-189.compute-1.amazonaws.com:8080'
 };
