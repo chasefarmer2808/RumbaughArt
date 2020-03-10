5c0f71032bd6e72721c58a5c019ba36d4958e7fd
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-03-09 22:34:46 -0400

    close menu on nav

--- rumbaugh-art-app/src/app/navbar/navbar.component.ts
+++ rumbaugh-art-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { LycheeService } from '../services/lychee/lychee.service';
 import { Album } from '../services/lychee/album';
 
@@ -12,12 +13,18 @@ export class NavbarComponent implements OnInit {
   galleries: Album[] = [];
   navActive: boolean = false;
 
-  constructor(private lycheeService: LycheeService) { }
+  constructor(private lycheeService: LycheeService, private router: Router) { }
 
   ngOnInit() {
     this.lycheeService.getPublicAlbums().subscribe((albums: Album[]) => {
       this.galleries = albums;
     });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.navActive = false;
+      }
+    })
   }
 
   toggleNav() {
