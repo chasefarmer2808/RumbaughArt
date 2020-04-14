2adbae416391bcd7e692deefa329d5bb21bd0002
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-04-13 23:17:32 -0400

    different size images in gallery

--- rumbaugh-art-app/src/app/gallery/gallery.component.ts
+++ rumbaugh-art-app/src/app/gallery/gallery.component.ts
@@ -21,7 +21,7 @@ export class GalleryComponent implements OnInit {
   // photoUrls: string[] = [];
   photoServerUrl: string = environment.photoServerUrl;
   // Make sure this thresh matches the minmax set in the css grid.
-  readonly photoThreshPx: number = 300;
+  readonly photoThreshPx: number = 550;
 
   constructor(private route: ActivatedRoute, 
     private lycheeService: LycheeService,
@@ -40,15 +40,20 @@ export class GalleryComponent implements OnInit {
   }
 
   refreshGrid(event, photoIndex:number) {
-    let imgParentElement = event.target.parentElement;
+    let imgParentElement = event.target.parentElement.parentElement;
     let photo:Photo = this.photos[photoIndex];
 
+    let dimentionDiff: number = photo.width - photo.height;
+
     // Make photo wider or taller in the grid based on its actual dimentions.
-    if (photo.width > this.photoThreshPx) {
+    if (dimentionDiff > this.photoThreshPx) {
       this.renderer.setStyle(imgParentElement, 'grid-column', 'span 2');
     } 
-    
-    if (photo.height > this.photoThreshPx) {
+    else if (dimentionDiff < (this.photoThreshPx * -1)) {
+      this.renderer.setStyle(imgParentElement, 'grid-row', 'span 2');
+    }
+    else {
+      this.renderer.setStyle(imgParentElement, 'grid-column', 'span 2');
       this.renderer.setStyle(imgParentElement, 'grid-row', 'span 2');
     }
   }
